f3a3cf31786567cbea83880767a36183ec04b1ea
Ric Allinson <allinson@yahoo-inc.com>
2016-08-02 17:20:02 -0700

    Added start of HTML for settings page.

--- webserver.go
+++ webserver.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/goforgery/forgery2"
+    "github.com/goforgery/mustache"
+    "github.com/goforgery/static"
 )
 
 // Starts the web server.
 func StartWebServer(port int) {
 
 	app := f.CreateApp()
+    app.Use(static.Create())
+    app.Engine(".html", mustache.Create())
+
 	zilla := CreateZilla(&MockPort{})
 
-	app.Get("/", func(req *f.Request, res *f.Response, next func()) {
-		res.Send(zilla)
+	app.Get("/settings", func(req *f.Request, res *f.Response, next func()) {
+		res.Render("settings.html", zilla)
 	})
 
 	fmt.Printf("The Manzanita Micro Zilla interface is now running on port '%d'.\n", port)
